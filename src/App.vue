3b3464636181149eccfc3e1634595597dd56e427
Valentin Sirbu <valentinsirbu@yandex.com>
2018-02-16 14:48:50 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
     <router-view/>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Template created</strong> by <a href="http://valentinsirbu.me">Valentin Sirbu</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
