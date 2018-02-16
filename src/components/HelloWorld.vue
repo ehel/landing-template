152cb42696fa3de65c81fa506aa5303b710020af
Valentin Sirbu <valentinsirbu@yandex.com>
2018-02-16 14:43:18 +0200

    Feature section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section class="hero is-primary is-fullheight is-bold">
+    <section class="hero is-info is-fullheight is-bold">
       <vue-particles color="#dedede" style="position: absolute; width: 100%; height: 100%;"></vue-particles>
       <div class="hero-body">
         <div class="container">
@@ -12,7 +12,7 @@
               <h2 class="subtitle font-regular-normal is-uppercase">
                 Full Height subtitle
               </h2>
-              <a class="button is-primary is-inverted is-outlined">Get Started</a>
+              <a class="button is-info is-inverted is-outlined">Get Started</a>
             </div>
             <div class="column">
               <img src="/static/imgs/Chrome-Browser-t.png" alt="">
@@ -24,7 +24,7 @@
     <section class="section">
       <div class="container has-text-centered">
         <h1 class="title font-accident-two-light is-uppercase mb-35">Section</h1>
-        <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-primary">
+        <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-info">
           A simple container to divide your page into sections, like the one you're currently reading
         </h2>
         <p class="font-regular-normal mb-35 is-size-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis earum iste laboriosam perferendis ratione unde voluptatum! Assumenda, dicta dignissimos ex excepturi illum natus necessitatibus, numquam officia pariatur quae reprehenderit veniam.</p>
@@ -32,8 +32,8 @@
           <svg width="0" height="0">
           <defs>
             <linearGradient id="primary">
-              <stop offset="5%" stop-color="#009e6c" />
-              <stop offset="95%" stop-color="#00d1b2" />
+              <stop offset="5%" stop-color="#04a6d7" />
+              <stop offset="95%" stop-color="#3287f5" />
             </linearGradient>
           </defs>
           </svg>
@@ -67,7 +67,7 @@
         </div>
       </div>
     </section>
-    <section class="hero section is-primary is-bold">
+    <section class="hero section is-info is-bold">
       <div class="container has-text-centered">
         <h1 class="title font-accident-two-light is-uppercase mb-35">Section</h1>
         <h2 class="subtitle font-accident-two-normal small is-uppercase">
@@ -79,12 +79,64 @@
     <section class="section">
       <div class="container has-text-centered">
         <h1 class="title font-accident-two-light is-uppercase mb-35">Features</h1>
-        <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-primary">
+        <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-info">
           A simple container to divide your page into sections, like the one you're currently reading
         </h2>
+        <div class="tile is-ancestor">
+          <div class="tile is-vertical is-8">
+            <div class="tile">
+              <div class="tile is-parent is-vertical">
+                <article class="tile is-child notification">
+                  <p class="title font-accident-two-medium">Feature</p>
+                  <p class="subtitle font-accident-two-normal">Subtitle</p>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi dicta dolorem eaque ex excepturi exercitationem fuga.
+                  </div>
+                </article>
+                <article class="tile is-child notification">
+                  <p class="title font-accident-two-medium">Features</p>
+                  <p class="subtitle font-accident-two-normal">Subtitle</p>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi dicta dolorem eaque ex excepturi exercitationem fuga.
+                  </div>
+                </article>
+              </div>
+              <div class="tile is-parent">
+                <article class="tile is-child notification">
+                  <p class="title font-accident-two-medium">Feature</p>
+                  <p class="subtitle font-accident-two-normal">Subtitle</p>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi dicta dolorem eaque ex excepturi exercitationem fuga laudantium magnam natus nesciunt nulla, quaerat, recusandae sunt ullam unde velit voluptates? Dolorum.
+                  </div>
+                </article>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child notification">
+                <p class="title font-accident-two-medium">Feature</p>
+                <p class="subtitle font-accident-two-normal">Subtitle</p>
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification">
+              <div class="content font-accident-two-normal">
+                <p class="title font-accident-two-medium">Feature</p>
+                <p class="subtitle">Subtitle</p>
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque neque odit quaerat sunt tempore, voluptas voluptates! Ab culpa delectus, deserunt eveniet facere possimus quisquam sunt totam vero voluptatibus. Amet, deleniti!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolor dolore nobis obcaecati, repellendus sapiente vero. Asperiores, blanditiis eos expedita magnam magni maxime modi molestias officiis quo, unde vitae, voluptatum.
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </section>
-    <section class="hero section is-primary is-bold">
+    <section class="hero section is-info is-bold">
       <div class="container has-text-centered">
         <h1 class="title font-accident-two-light is-uppercase mb-35">Contact Us</h1>
         <h2 class="subtitle font-accident-two-normal small is-uppercase">
@@ -105,9 +157,8 @@
                   </span>
               </div>
               <div class="control">
-                <input type="submit" value="Subscribe" class="button is-white is-outlined is-pulled-right">
+                <input type="submit" value="Subscribe" class="button is-info is-inverted is-outlined is-pulled-right">
               </div>
-
             </form>
           </div>
         </div>
