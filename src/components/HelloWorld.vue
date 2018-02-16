9b1c565cf93f1819cf984ad0ffc709444f6fc83a
Valentin Sirbu <valentinsirbu@yandex.com>
2018-02-16 14:19:11 +0200

    Add contact form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@
               <h2 class="subtitle font-regular-normal is-uppercase">
                 Full Height subtitle
               </h2>
+              <a class="button is-primary is-inverted is-outlined">Get Started</a>
             </div>
             <div class="column">
               <img src="/static/imgs/Chrome-Browser-t.png" alt="">
@@ -26,36 +27,40 @@
         <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-primary">
           A simple container to divide your page into sections, like the one you're currently reading
         </h2>
-        <p class="font-regular-normal mb-35">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis earum iste laboriosam perferendis ratione unde voluptatum! Assumenda, dicta dignissimos ex excepturi illum natus necessitatibus, numquam officia pariatur quae reprehenderit veniam.</p>
+        <p class="font-regular-normal mb-35 is-size-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis earum iste laboriosam perferendis ratione unde voluptatum! Assumenda, dicta dignissimos ex excepturi illum natus necessitatibus, numquam officia pariatur quae reprehenderit veniam.</p>
         <div class="columns">
+          <svg width="0" height="0">
+          <defs>
+            <linearGradient id="primary">
+              <stop offset="5%" stop-color="#009e6c" />
+              <stop offset="95%" stop-color="#00d1b2" />
+            </linearGradient>
+          </defs>
+          </svg>
           <div class="column">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40%" height="40%" fill="url(#primary)">
-              <linearGradient id="primary">
-                <stop offset="5%" stop-color="#009e6c" />
-                <stop offset="95%" stop-color="#00d1b2" />
-              </linearGradient>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30%" height="30%" fill="url(#primary)">
               <path class="heroicon-ui" d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"/>
             </svg>
             <h3 class="subtitle font-accident-two-medium is-uppercase">First</h3>
-            <p class="font-regular-normal small">
+            <p class="font-regular-normal is-size-7">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores dicta fugiat modi nam natus quasi recusandae repellendus ullam voluptate? Ad aspernatur consequatur dolores nulla obcaecati quasi reprehenderit similique suscipit.
             </p>
           </div>
           <div class="column">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40%" height="40%" fill="url(#primary)">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30%" height="30%" fill="url(#primary)">
               <path class="heroicon-ui" d="M9.57 3.38a8 8 0 0 0 10.4 10.4 1 1 0 0 1 1.31 1.3 10 10 0 1 1-13-13 1 1 0 0 1 1.3 1.3zM7.1 5.04A8 8 0 1 0 18.3 16.27 10 10 0 0 1 7.08 5.04z"/>
             </svg>
             <h3 class="subtitle font-accident-two-medium is-uppercase">Second</h3>
-            <p class="font-regular-normal small">
+            <p class="font-regular-normal is-size-7">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores dicta fugiat modi nam natus quasi recusandae repellendus ullam voluptate? Ad aspernatur consequatur dolores nulla obcaecati quasi reprehenderit similique suscipit.
             </p>
           </div>
           <div class="column">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40%" height="40%" fill="url(#primary)">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30%" height="30%" fill="url(#primary)">
               <path stroke-width="1" class="heroicon-ui" d="M17 22a2 2 0 0 1-2-2v-1a1 1 0 0 0-1-1 1 1 0 0 0-1 1v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-3H5a3 3 0 1 1 0-6h1V8c0-1.11.9-2 2-2h3V5a3 3 0 1 1 6 0v1h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1a1 1 0 0 0-1 1 1 1 0 0 0 1 1h1a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-3zm3-2v-3h-1a3 3 0 1 1 0-6h1V8h-3a2 2 0 0 1-2-2V5a1 1 0 0 0-1-1 1 1 0 0 0-1 1v1a2 2 0 0 1-2 2H8v3a2 2 0 0 1-2 2H5a1 1 0 0 0-1 1 1 1 0 0 0 1 1h1a2 2 0 0 1 2 2v3h3v-1a3 3 0 1 1 6 0v1h3z"/>
             </svg>
             <h3 class="subtitle font-accident-two-medium is-uppercase">Third</h3>
-            <p class="font-regular-normal small">
+            <p class="font-regular-normal is-size-7">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores dicta fugiat modi nam natus quasi recusandae repellendus ullam voluptate? Ad aspernatur consequatur dolores nulla obcaecati quasi reprehenderit similique suscipit.
             </p>
           </div>
@@ -68,7 +73,44 @@
         <h2 class="subtitle font-accident-two-normal small is-uppercase">
           A simple container to divide your page into sections, like the one you're currently reading
         </h2>
-        <p class="font-regular-normal mb-35">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis earum iste laboriosam perferendis ratione unde voluptatum! Assumenda, dicta dignissimos ex excepturi illum natus necessitatibus, numquam officia pariatur quae reprehenderit veniam.</p>
+        <p class="font-regular-normal mb-35 is-size-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis earum iste laboriosam perferendis ratione unde voluptatum! Assumenda, dicta dignissimos ex excepturi illum natus necessitatibus, numquam officia pariatur quae reprehenderit veniam.</p>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container has-text-centered">
+        <h1 class="title font-accident-two-light is-uppercase mb-35">Features</h1>
+        <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-primary">
+          A simple container to divide your page into sections, like the one you're currently reading
+        </h2>
+      </div>
+    </section>
+    <section class="hero section is-primary is-bold">
+      <div class="container has-text-centered">
+        <h1 class="title font-accident-two-light is-uppercase mb-35">Contact Us</h1>
+        <h2 class="subtitle font-accident-two-normal small is-uppercase">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dignissimos laborum tenetur veniam voluptas!
+        </h2>
+        <div class="colums">
+          <div class="column is-half is-offset-one-quarter">
+            <form action="#">
+              <div class="control has-icons-left is-expanded">
+                <div class="field">
+                  <input type="email" value="" name="email" class="input is-flat required email" placeholder="email address">
+                </div>
+                <div class="field">
+                  <textarea class="textarea" placeholder="Hello world"></textarea>
+                </div>
+                <span class="icon is-small is-left">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#dedede"><path class="heroicon-ui" d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2zm16 3.38V6H4v1.38l8 4 8-4zm0 2.24l-7.55 3.77a1 1 0 0 1-.9 0L4 9.62V18h16V9.62z"/></svg>
+                  </span>
+              </div>
+              <div class="control">
+                <input type="submit" value="Subscribe" class="button is-white is-outlined is-pulled-right">
+              </div>
+
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   </div>
