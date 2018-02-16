94f74bc3e8df917895517aa11c4b6c88dc23fce1
Valentin Sirbu <valentinsirbu@yandex.com>
2018-02-16 15:02:30 +0200

    Testemonials

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -136,6 +136,94 @@
         </div>
       </div>
     </section>
+    <section class="hero section is-light is-bold">
+      <div class="container has-text-centered">
+        <h1 class="title font-accident-two-light is-uppercase mb-35">Testemonials</h1>
+        <h2 class="subtitle font-accident-two-normal small is-uppercase has-text-info">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dignissimos laborum tenetur veniam voluptas!
+        </h2>
+        <p class="small fontcolor-medium text-center mb-35">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Accusamus aliquid beatae, deserunt esse ex, explicabo hic illum incidunt maxime, possimus sed suscipit vitae. <br> Dignissimos, enim laborum nihil nulla sequi unde.
+        </p>
+        <div class="columns">
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img class="is-rounded" src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4 font-accident-two-medium">John Smith</p>
+                    <p class="subtitle is-6 font-accident-two-light">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content font-regular-normal">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                  <br>
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img class="is-rounded" src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4 font-accident-two-medium">John Smith</p>
+                    <p class="subtitle is-6 font-accident-two-light">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content font-regular-normal">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                  <br>
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img class="is-rounded" src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4 font-accident-two-medium">John Smith</p>
+                    <p class="subtitle is-6 font-accident-two-light">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content font-regular-normal">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                  <br>
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <section class="hero section is-info is-bold">
       <div class="container has-text-centered">
         <h1 class="title font-accident-two-light is-uppercase mb-35">Contact Us</h1>
