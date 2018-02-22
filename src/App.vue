f95e78259497854efdd2f1c62e36bb3e5aae1bce
Valentin Sirbu <valentinsirbu@yandex.com>
2018-02-22 19:18:25 +0200

    add slick carousel

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,12 @@
             <a class="navbar-item" href="/">
               <img src="/static/imgs/logo.png" height="28px">
             </a>
-            <a href="/" class="navbar-item font-regular-normal has-text-grey-darker is-active">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path class="heroicon-ui" d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"/></svg>
+            <a href="/" class="navbar-item font-regular-medium is-uppercase has-text-grey-darker is-active">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12"><path class="heroicon-ui" d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"/></svg>
               Home</a>
+            <a href="#" class="navbar-item">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+            </a>
                 <span class="navbar-burger burger" :class="{'is-active': burgerOpen}" @click="burgerOpen = !burgerOpen">
                   <span></span>
                   <span></span>
@@ -18,15 +21,15 @@
           </div>
           <div class="navbar-menu" :class="{'is-active': burgerOpen}">
             <div class="navbar-end">
-              <a href="#" class="navbar-item font-regular-normal has-text-grey-darker">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path class="heroicon-ui" d="M20 9v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2zm0-2V5H4v2h16zM6 9v10h12V9H6zm4 2h4a1 1 0 0 1 0 2h-4a1 1 0 0 1 0-2z"/></svg>
+              <a href="#" class="navbar-item font-regular-medium is-uppercase  has-text-grey-darker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12"><path class="heroicon-ui" d="M20 9v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2zm0-2V5H4v2h16zM6 9v10h12V9H6zm4 2h4a1 1 0 0 1 0 2h-4a1 1 0 0 1 0-2z"/></svg>
                 Examples</a>
-              <a href="#" class="navbar-item font-regular-normal has-text-grey-darker">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path class="heroicon-ui" d="M20 6a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h7.41l2 2H20zM4 6v12h16V8h-7.41l-2-2H4z"/></svg>
+              <a href="#" class="navbar-item font-regular-medium is-uppercase has-text-grey-darker">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12"><path class="heroicon-ui" d="M20 6a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h7.41l2 2H20zM4 6v12h16V8h-7.41l-2-2H4z"/></svg>
                 Docs</a>
               <span class="navbar-item">
-                  <a href="#" class="button is-info is-outlined" :class="{'is-fullwidth': burgerOpen}">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16"><path class="heroicon-ui" d="M19 10h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2h-2a1 1 0 0 1 0-2h2V8a1 1 0 0 1 2 0v2zM9 12A5 5 0 1 1 9 2a5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm8 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H7a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h5a5 5 0 0 1 5 5v2z"/></svg>
+                  <a href="#" class="button is-info font-regular-medium is-uppercase is-outlined" :class="{'is-fullwidth': burgerOpen}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12"><path class="heroicon-ui" d="M19 10h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2h-2a1 1 0 0 1 0-2h2V8a1 1 0 0 1 2 0v2zM9 12A5 5 0 1 1 9 2a5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm8 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H7a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h5a5 5 0 0 1 5 5v2z"/></svg>
                 Sign Up
                   </a>
                 </span>
@@ -92,6 +95,11 @@
    }
    .navbar-item{
      transition: 300ms ease color;
+     font-size: 12px;
+     a{
+       padding: 15px 20px;
+       font-size: 12px;
+     }
      svg{
        margin-right: 5px;
        fill: hsl(0, 0%, 21%);
--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -8,37 +8,50 @@
       <p class="small font-regular-normal text-center mb-35">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Accusamus aliquid beatae, deserunt esse ex, explicabo hic illum incidunt maxime, possimus sed suscipit vitae. <br> Dignissimos, enim laborum nihil nulla sequi unde.
       </p>
-      <div class="columns">
-        <div class="column" v-for="testimonial in testimonials" :key="testimonial.id">
-          <div class="card">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image is-48x48">
-                    <img class="is-rounded" :src="testimonial.image" alt="Placeholder image">
-                  </figure>
+      <div>
+        <slick class="columns" :options="options">
+          <div class="column" v-for="testimonial in testimonials" :key="testimonial.id">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img class="is-rounded" :src="testimonial.image" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content" style="overflow: hidden">
+                    <p class="title is-4 font-accident-two-medium" v-text="testimonial.name"></p>
+                    <p class="subtitle is-6 font-accident-two-light has-text-info" v-text="testimonial.company"></p>
+                  </div>
+                  <div class="media-right">
+                    <p class="is-size-7 font-accident-two-normal is-uppercase" v-text="testimonial.date"></p>
+                  </div>
                 </div>
-                <div class="media-content" style="overflow: hidden">
-                  <p class="title is-4 font-accident-two-medium" v-text="testimonial.name"></p>
-                  <p class="subtitle is-6 font-accident-two-light has-text-info" v-text="testimonial.company"></p>
-                </div>
-                <div class="media-right">
-                  <p class="is-size-7 font-accident-two-normal is-uppercase" v-text="testimonial.date"></p>
-                </div>
+                <div class="content font-regular-normal" v-text="testimonial.message"></div>
               </div>
-              <div class="content font-regular-normal" v-text="testimonial.message"></div>
             </div>
           </div>
-        </div>
+        </slick>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+  import Slick from './Slick.vue'
   export default {
+    components: {
+      Slick
+    },
     data () {
       return {
+        options: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          arrows: false
+        },
         testimonials: [
           {
             id: 1,
@@ -62,6 +75,30 @@
             image: 'https://bulma.io/images/placeholders/96x96.png',
             company: 'Amazon',
             date: '3 Jan 2018',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n Phasellus nec iaculis mauris.'
+          },
+          {
+            id: 4,
+            name: 'Alex Doe',
+            image: 'https://bulma.io/images/placeholders/96x96.png',
+            company: 'eBay',
+            date: '4 Jan 2018',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n Phasellus nec iaculis mauris.'
+          },
+          {
+            id: 5,
+            name: 'Alan Doe',
+            image: 'https://bulma.io/images/placeholders/96x96.png',
+            company: 'Microsoft',
+            date: '5 Jan 2018',
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n Phasellus nec iaculis mauris.'
+          },
+          {
+            id: 6,
+            name: 'Jane Doe',
+            image: 'https://bulma.io/images/placeholders/96x96.png',
+            company: 'Apple',
+            date: '6 Jan 2018',
             message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n Phasellus nec iaculis mauris.'
           }
         ]
