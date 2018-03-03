6317d5bd0c46da2e34c258c17ed1f08c24e209e0
Valentin Sirbu <valentinsirbu@yandex.com>
2018-03-03 11:29:18 +0200

    Update projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -12,22 +12,52 @@
         <div class="tile is-parent">
           <article class="tile is-child notification first-project project project-vertical">
             <div class="content">
-              <p class="title font-accident-two-medium is-uppercase is-size-5 mb-35">Project Title</p>
-              <p class="subtitle is-size-7 font-accident-two-normal is-uppercase">Project Description</p>
+              <div class="project-header">
+                <p class="title font-accident-two-medium is-uppercase is-size-5 mb-35">Project Title</p>
+                <p class="subtitle is-size-7 font-accident-two-normal is-uppercase">Project Subtitle</p>
+              </div>
+              <div class="project-body">
+                <p class="small font-regular-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi dignissimos distinctio doloremque, dolores eius est fugiat illum magni natus nobis quasi quidem quod repellat soluta tempora tempore ut, veritatis?</p>
+              </div>
+              <div class="project-footer">
+                <a href="#">5 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M19.48 13.03A4 4 0 0 1 16 19h-4a4 4 0 1 1 0-8h1a1 1 0 0 0 0-2h-1a6 6 0 1 0 0 12h4a6 6 0 0 0 5.21-8.98L21.2 12a1 1 0 1 0-1.72 1.03zM4.52 10.97A4 4 0 0 1 8 5h4a4 4 0 1 1 0 8h-1a1 1 0 0 0 0 2h1a6 6 0 1 0 0-12H8a6 6 0 0 0-5.21 8.98l.01.02a1 1 0 1 0 1.72-1.03z"/></svg></a>
+                <a href="#">23 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M17.62 10H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H8.5c-1.2 0-2.3-.72-2.74-1.79l-3.5-7-.03-.06A3 3 0 0 1 5 9h5V4c0-1.1.9-2 2-2h1.62l4 8zM16 11.24L12.38 4H12v7H5a1 1 0 0 0-.93 1.36l3.5 7.02a1 1 0 0 0 .93.62H16v-8.76zm2 .76v8h2v-8h-2z"/></svg></a>
+                <a href="#" class="font-regular-normal">12 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/></svg></a>
+              </div>
             </div>
           </article>
         </div>
         <div class="tile is-parent is-vertical is-8">
           <article class="tile is-child notification second-project project project-horizontal">
             <div class="content">
-              <p class="title font-accident-two-medium is-uppercase is-size-5 mb-35">Project Title</p>
-              <p class="subtitle is-size-7 font-accident-two-normal is-uppercase">Project Description</p>
+              <div class="project-header">
+                <p class="title font-accident-two-medium is-uppercase is-size-5 mb-35">Project Title</p>
+                <p class="subtitle is-size-7 font-accident-two-normal is-uppercase">Project Subtitle</p>
+              </div>
+              <div class="project-body">
+                <p class="small font-regular-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi dignissimos distinctio doloremque, dolores eius est fugiat illum magni natus nobis quasi quidem quod repellat soluta tempora tempore ut, veritatis?</p>
+              </div>
+              <div class="project-footer">
+                <a href="#">5 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M19.48 13.03A4 4 0 0 1 16 19h-4a4 4 0 1 1 0-8h1a1 1 0 0 0 0-2h-1a6 6 0 1 0 0 12h4a6 6 0 0 0 5.21-8.98L21.2 12a1 1 0 1 0-1.72 1.03zM4.52 10.97A4 4 0 0 1 8 5h4a4 4 0 1 1 0 8h-1a1 1 0 0 0 0 2h1a6 6 0 1 0 0-12H8a6 6 0 0 0-5.21 8.98l.01.02a1 1 0 1 0 1.72-1.03z"/></svg></a>
+                <a href="#">23 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M17.62 10H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H8.5c-1.2 0-2.3-.72-2.74-1.79l-3.5-7-.03-.06A3 3 0 0 1 5 9h5V4c0-1.1.9-2 2-2h1.62l4 8zM16 11.24L12.38 4H12v7H5a1 1 0 0 0-.93 1.36l3.5 7.02a1 1 0 0 0 .93.62H16v-8.76zm2 .76v8h2v-8h-2z"/></svg></a>
+                <a href="#" class="font-regular-normal">12 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/></svg></a>
+              </div>
             </div>
           </article>
           <article class="tile is-child notification third-project project project-horizontal">
             <div class="content">
-              <p class="title font-accident-two-medium is-uppercase is-size-5 mb-35">Project Title</p>
-              <p class="subtitle is-size-7 font-accident-two-normal is-uppercase">Top tile</p>
+              <div class="project-header">
+                <p class="title font-accident-two-medium is-uppercase is-size-5 mb-35">Project Title</p>
+                <p class="subtitle is-size-7 font-accident-two-normal is-uppercase">Project Subtitle</p>
+              </div>
+              <div class="project-body">
+                <p class="small font-regular-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi dignissimos distinctio doloremque, dolores eius est fugiat illum magni natus nobis quasi quidem quod repellat soluta tempora tempore ut, veritatis?</p>
+              </div>
+              <div class="project-footer">
+                <a href="#">5 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M19.48 13.03A4 4 0 0 1 16 19h-4a4 4 0 1 1 0-8h1a1 1 0 0 0 0-2h-1a6 6 0 1 0 0 12h4a6 6 0 0 0 5.21-8.98L21.2 12a1 1 0 1 0-1.72 1.03zM4.52 10.97A4 4 0 0 1 8 5h4a4 4 0 1 1 0 8h-1a1 1 0 0 0 0 2h1a6 6 0 1 0 0-12H8a6 6 0 0 0-5.21 8.98l.01.02a1 1 0 1 0 1.72-1.03z"/></svg></a>
+                <a href="#">23 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M17.62 10H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H8.5c-1.2 0-2.3-.72-2.74-1.79l-3.5-7-.03-.06A3 3 0 0 1 5 9h5V4c0-1.1.9-2 2-2h1.62l4 8zM16 11.24L12.38 4H12v7H5a1 1 0 0 0-.93 1.36l3.5 7.02a1 1 0 0 0 .93.62H16v-8.76zm2 .76v8h2v-8h-2z"/></svg></a>
+                <a href="#" class="font-regular-normal">12 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14"><path class="heroicon-ui" d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"/></svg></a>
+              </div>
             </div>
           </article>
         </div>
@@ -56,6 +86,19 @@
       right: 0;
       bottom: 0;
       color: #fff;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .project-footer{
+        display: flex;
+        justify-content: space-around;
+        a{
+          text-decoration: none;
+        }
+        svg{
+          fill: #fff;
+        }
+      }
     }
     &:hover{
       .content{
